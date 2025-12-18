979698879bf2083519d8547d229d0ba409505ea0
Panom M <p.makuei@alustudent.com>
2025-12-18 12:19:38 +0200

    staeting to edit  course work 3

---
+++ Coursework 3/kgl_analytics.js
@@ -0,0 +1,33 @@
+/*
+==============================================
+PART A: Higher Order Functions - Data Transformation
+==============================================
+*/
+
+function procurementRecords (id,name,type,tonnage,cost,){
+    return { 
+        id: number, 
+        dealerName: string, 
+        produceType: string, 
+        tonnageInKgs: number, 
+        costInUgx: number, 
+        procurementDate: new Date
+     } 
+
+}
+
+
+/*
+==============================================
+PART B: Sets for Unique Data Management
+==============================================
+*/
+
+/*
+==============================================
+PART c: Maps for Price Management
+==============================================
+*/
+
+
+
